b13f0b35b625f0a0bb28d05658ef88856e4eb496
Everett Griffiths <everett@fireproofsocks.com>
2015-03-17 00:07:46 -0700

    Working on import... still need to do meta fields, page terms, and shortcodes and URL cleanup

--- src/Console/Command/ImportWpCommand.php
+++ src/Console/Command/ImportWpCommand.php
@@ -16,6 +16,8 @@ class ImportWpCommand extends Command
     public $modx;
 
     public $data;
+    public $map;
+
     public $valid_mappings = array(
         'post_types',
         'default_templates',
@@ -24,6 +26,15 @@ class ImportWpCommand extends Command
         'shortcodes',
         'fields'
     );
+
+    public $xml_nodes = array('version','base_url','authors','posts');
+
+    // Lookups: name --> id (cut down on redundant lookups)
+    public $cat_lookup = array();
+    public $tag_lookup = array();
+    public $auth_lookup = array();
+    public $url_lookup = array();
+    public $wp_modx_ids_lookup = array();
 
     protected function configure()
     {
@@ -48,6 +59,9 @@ class ImportWpCommand extends Command
     {
         $this->modx = get_modx();
         $this->modx->initialize('mgr');
+
+        // TODO: test whether assetmanager and taxonomies are installed
+
         $this->resource_cols = $this->modx->getFields('modResource');
 
         $source = $input->getArgument('source');
@@ -75,24 +89,26 @@ class ImportWpCommand extends Command
         }
 
         $yaml = new Parser();
-        $map = $yaml->parse(file_get_contents($mapfile));
+        $this->map = $yaml->parse(file_get_contents($mapfile));
         $output->writeln('Importing file '.$source. ' using mappings contained in '.$mapfile);
 
 
 
         $WP = new \Xls2modx\Parser\WordPressXml();
         $this->data = $WP->parse($source);
-
+//print_r($this->data); exit;
         // Verify mappings
-        $this->verifyMappings($map, $output);
-        $this->verifyXML($output);
+        $this->verifyMappings($this->map, $output);
+        $this->verifyXML($this->data, $output);
 
-        // Check mapped authors: create any users if necessary (LOG THEM!)
-        // Check mapped fields : create any TVs if necessary (LOG THEM!)
-        // Import/Create any taxonomies/terms
+        $this->createCategories($output);
+        $this->createTags($output);
+        $this->createAuthors($output);
 
         // Iterate over posts
-        // build lookup table for URLs
+        $this->createContent($output);
+        exit;
+        // Check mapped fields : create any TVs if necessary (LOG THEM!)
         // check for custom fields containing multiple rows of data
         // add any page/term associations
         // Import assets
@@ -104,119 +120,279 @@ class ImportWpCommand extends Command
 
     }
 
-    public function createModxResource($vals,$headers,$fullmap, $output)
+    /**
+     *  Array
+    (
+    [term_id] => 2
+    [category_nicename] => dork
+    [category_parent] =>
+    [cat_name] => Dork
+    [category_description] =>
+    )
+     * @throws \Exception
+     */
+    public function createCategories($output)
     {
-        //$R = $this->modx->newObject('modResource');
-        $data = array();
-        $tvdata = array();
-        $map = $fullmap['xls2modx'];
-        foreach ($vals as $i => $v)
+        // Find parent Taxonomy
+        if (!$Tax = $this->modx->getObject('Taxonomy', array('alias'=>'categories')))
         {
-            //print_r($headers); exit;
-            //print $v; exit;
-            if (isset($map[ $headers[$i] ]))
-            {
-                if (empty($map[ $headers[$i] ])) continue;
+            //throw new \Exception('Taxonomy for "categories" not found.');
+            $Tax = $this->modx->newObject('Taxonomy');
 
-                if (!is_array($map[ $headers[$i] ]))
+            $Tax->set('pagetitle', 'Categories');
+            $Tax->set('menutitle', 'Categories');
+            $Tax->set('alias', 'categories');
+            $Tax->set('published', true);
+            $Tax->set('template', $this->modx->getOption('taxonomies.default_taxonomy_template', null, $this->modx->getOption('default_template')));
+
+            $Tax->save();
+            $output->writeln('Creating parent taxonomy: categories');
+        }
+        else
+        {
+            $output->writeln('Found existing taxonomy: categories');
+        }
+
+        foreach ($this->data['categories'] as $c)
+        {
+            // Warning: this may fail if there are both a TERM and a CATEGORY using the same alias slug!
+            if (!$Term = $this->modx->getObject('Term', array('alias' => $c['category_nicename'])))
+            {
+                $Term = $this->modx->newObject('Term');
+                if ($c['category_parent'])
                 {
-                    $columns = array($map[ $headers[$i] ]);
+                    if ($ParentTerm = $this->modx->getObject('Term', array('alias' => $c['category_parent'],'parent'=>$Tax->get('id'))))
+                    {
+                        $Term->set('parent', $ParentTerm->get('id'));
+                    }
                 }
                 else
                 {
-                    $columns = $map[ $headers[$i] ];
+                    $Term->set('parent', $Tax->get('id'));
                 }
+                $Term->set('pagetitle', $c['cat_name']);
+                $Term->set('menutitle', $c['cat_name']);
+                $Term->set('description', $c['category_description']);
+                $Term->set('alias', $c['category_nicename']);
+                $Term->set('published', true);
+                $Term->set('template', $this->modx->getOption('taxonomies.default_term_template', null, $this->modx->getOption('default_template')));
 
-                foreach ($columns as $col)
-                {
-                    // Regular resource col
-                    if (array_key_exists($col, $this->resource_cols))
-                    {
-                        $data[ $col ] = $v; // TODO: do some crunching before simple handoff
-                    }
-                    // TV
-                    else
-                    {
-                        $tvdata[ $col ] = $v;
-                    }
-                }
+                $Term->save();
+                $output->writeln('Creating category term: '.$c['cat_name']);
+
             }
-        }
-        foreach($fullmap['Hardcoded-Values'] as $k => $v)
-        {
-            if (empty($v)) continue;
-            if (array_key_exists($col, $this->resource_cols))
-            {
-                $data[ $k ] = $v;
-            }
-            // TV
             else
             {
-                $tvdata[ $k ] = $v;
+                $output->writeln('Found existing term: '.$c['category_nicename']);
             }
-        }
-//        print_r($data);
-//        print_r($tvdata);
-//        exit;
-        // Record Exists?
-        $idcol = $fullmap['Config']['identifier'];
-        if (!$Resource = $this->modx->getObject('modResource', array($idcol => $data[$idcol])))
-        {
-            $Resource = $this->modx->newObject('modResource');
-        }
-        foreach ($data as $k => $v)
-        {
-            // TODO : check for overwrite
-            $Resource->set($k, $v);
-        }
 
-        // Generate alias
-        if (!$alias = $Resource->get('alias'))
-        {
-            $alias = $Resource->cleanAlias($Resource->get('pagetitle'));
-            //$friendly_urls = $this->modx->getOption('friendly_urls');
-            $aliasPath = $Resource->getAliasPath($alias, $Resource->toArray());
-            $Resource->set('alias', $alias);
-            $Resource->set('uri', $aliasPath);
-//            print $alias ."\n";
-//            print $aliasPath ."\n";
-//            exit;
+            $this->cat_lookup[ $c['category_nicename'] ] = $Term->get('id');
         }
+    }
 
 
-        if(!$Resource->save())
+    /**
+     * Array
+    (
+    [term_id] => 5
+    [tag_slug] => random
+    [tag_name] => random
+    [tag_description] =>
+    )
+     * @param $output
+     */
+    public function createTags($output)
+    {
+        // Find parent Taxonomy
+        if (!$Tax = $this->modx->getObject('Taxonomy', array('alias'=>'tags')))
         {
-            $output->writeln('<error>There was a problem creating Resource</error>');
-            return;
-        }
-        $output->writeln('<info>Created resource '.$data[$idcol].'</info>');
+            //throw new \Exception('Taxonomy for "categories" not found.');
+            $Tax = $this->modx->newObject('Taxonomy');
 
-        foreach($tvdata as $name => $val)
+            $Tax->set('pagetitle', 'Tags');
+            $Tax->set('menutitle', 'Tags');
+            $Tax->set('alias', 'tags');
+            $Tax->set('published', true);
+            $Tax->set('template', $this->modx->getOption('taxonomies.default_taxonomy_template', null, $this->modx->getOption('default_template')));
+
+            $Tax->save();
+            $output->writeln('Creating parent taxonomy: tags');
+        }
+        else
         {
-            if (!isset($this->tvname_to_id[$name]))
+            $output->writeln('Parent taxonomy found: tags');
+        }
+
+        foreach ($this->data['tags'] as $t)
+        {
+            // Warning: this may fail if there are both a TERM and a CATEGORY using the same alias slug!
+            if (!$Term = $this->modx->getObject('Term', array('alias' => $t['tag_slug'])))
             {
-                $output->writeln('<error>TV not defined: '.$name.'</error>');
+                $Term = $this->modx->newObject('Term');
+
+                $Term->set('parent', $Tax->get('id'));
+                $Term->set('pagetitle', $t['tag_name']);
+                $Term->set('menutitle', $t['tag_name']);
+                $Term->set('description', $t['tag_description']);
+                $Term->set('alias', $t['tag_slug']);
+                $Term->set('published', true);
+                $Term->set('template', $this->modx->getOption('taxonomies.default_term_template', null, $this->modx->getOption('default_template')));
+
+                $Term->save();
+                $output->writeln('Creating tag term: '.$t['tag_name']);
+
+            }
+            else
+            {
+                $output->writeln('Found existing term: '.$t['tag_name']);
+            }
+
+            $this->tag_lookup[ $t['tag_slug'] ] = $Term->get('id');
+        }
+
+
+    }
+
+    /**
+     * [admin] => Array
+    (
+    [author_id] => 1
+    [author_login] => admin
+    [author_email] => everett@fireproofsocks.com
+    [author_display_name] => admin
+    [author_first_name] =>
+    [author_last_name] =>
+    )
+     * @param $output
+     */
+    public function createAuthors($output)
+    {
+        foreach ($this->data['authors'] as $username => $a)
+        {
+            if (!$U = $this->modx->getObject('modUser', array('username'=>$a['author_login'])))
+            {
+                $U = $this->modx->newObject('modUser');
+                $P = $this->modx->newObject('modUserProfile');
+                $U->set('username', $a['author_login']);
+                $P->set('email', $a['author_email']);
+                $P->set('fullname', trim($a['author_first_name'].' '.$a['author_last_name']));
+                $U->addOne($P);
+                $U->save();
+
+                $output->writeln('Creating user: '.$a['author_login']);
+            }
+            else
+            {
+                $output->writeln('Found existing user: '.$a['author_login']);
+            }
+
+            $this->auth_lookup[ $a['author_login'] ] = $U->get('id');
+        }
+    }
+
+
+    public function createContent($output)
+    {
+        foreach ($this->data['posts'] as $p) {
+
+            if (!$P = $this->modx->getObject('modResource', array('alias' => $p['post_name']))) {
+                $P = $this->modx->newObject('modResource');
+                $P->set('alias', $p['post_name']);
+            }
+
+//            foreach ($this->map['fields'] as $wp => $mx)
+//            {
+//                if (!$mx)
+//                {
+//                    continue;
+//                }
+//                if (is_array($mx))
+//                {
+//                    foreach ($mx as $m)
+//                    {
+//                        $P->set($m, $p[ $wp ]);
+//                    }
+//                }
+//                else
+//                {
+//                    $P->set($mx, $p[ $wp ]);
+//                }
+//            }
+
+            if ($p['post_type'] == 'attachment')
+            {
+                // TODO: create asset!
                 continue;
             }
-            $tmplvarid = $this->tvname_to_id[$name];
 
-            if (!$TVR = $this->modx->getObject('modTemplateVarResource', array('contentid'=>$Resource->get('id'), 'tmplvarid'=>$tmplvarid)))
+            $P->set('pagetitle', $p['post_title']);
+            $P->set('longtitle', $p['post_title']);
+            $P->set('introtext', $p['post_excerpt']);
+            $P->set('createdby', $this->auth_lookup[ $p['post_author'] ]);
+            $P->set('createdon', strtotime($p['post_date_gmt']));
+            if ($p['post_parent'] && isset($this->wp_modx_ids_lookup[ $p['post_parent'] ]))
             {
-                $TVR = $this->modx->newObject('modTemplateVarResource');
-                $TVR->set('contentid', $Resource->get('id'));
-                $TVR->set('tmplvarid', $tmplvarid);
+                $P->set('parent', $this->wp_modx_ids_lookup[ $p['post_parent'] ]);
             }
-            $TVR->set('value', $val);
+            if (isset($this->map['post_types'][ $p['post_type'] ]) && $this->map['post_types'][ $p['post_type'] ])
+            {
+                $P->set('class_key', $this->map['post_types'][ $p['post_type'] ]);
+            }
 
-            if (!$TVR->save())
+            if (isset($this->map['default_templates'][ $p['post_type'] ]) && $this->map['default_templates'][ $p['post_type'] ])
             {
-                $output->writeln('<error>Error saving TV: '.$name.'</error>');
-                continue;
+                // We may override this later via the custom fields
+                $P->set('template', $this->map['default_templates'][ $p['post_type'] ]);
             }
+            else
+            {
+                $P->set('template', $this->modx->getOption('default_template'));
+            }
+            if ($p['status'] == 'publish')
+            {
+                $P->set('published', true);
+            }
+
+
+            // Content
+            $content = $p['post_content'];
+
+            // Shortcodes?
+            // Urls?
+            // nl2br
+            $P->set('content', $content);
+
+
+            $P->save();
+            $output->writeln('Creating page: '.$P->get('pagetitle').' ('.$P->get('id').')');
+
+            $this->url_lookup[ $p['guid'] ] = $P->get('id');
+            $this->wp_modx_ids_lookup[ $p['post_id'] ] = $P->get('id');
         }
 
-//        print_r($data);
-//        print_r($tvdata);
+    }
+
+    /**
+     * Using Asset Manager, create the asset record (must have moved stuff into place first?)
+     */
+    public function createAsset()
+    {
+
+    }
+
+    /**
+     * Handle shortcodes, urls, nl2br
+     */
+    public function massageContent()
+    {
+
+    }
+
+    /**
+     * Associate a page with any terms
+     */
+    public function addPageTerms()
+    {
 
     }
 
@@ -247,15 +423,24 @@ class ImportWpCommand extends Command
     public function verifyMappings($map, $output)
     {
 
-        if (isset($map['']))
+        foreach ($map as $k => $v)
         {
-
+            if (!in_array($k, $this->valid_mappings))
+            {
+                throw new \Exception('Invalid mapping YML. Unrecognized node '. $k);
+            }
         }
     }
 
-    public function verifyXML($output)
+    public function verifyXML($data, $output)
     {
-
+        foreach ($this->xml_nodes as $n)
+        {
+            if (!isset($data[$n]))
+            {
+                throw new \Exception('Invalid XML: missing node '. $n);
+            }
+        }
     }
 
     public function mapTvNames2id()
